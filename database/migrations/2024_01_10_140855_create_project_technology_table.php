533bfe847ad0c826d57bf440083a41b553a164aa
SinghAmandeep <amandeepsingh3095@gmail.com>
2024-01-10 15:46:08 +0100

    solved issue with names

--- database/migrations/2024_01_10_140855_create_project_technology_table.php
+++ database/migrations/2024_01_10_140855_create_project_technology_table.php
@@ -18,10 +18,10 @@ return new class extends Migration
 
             $table->foreign('project_id')->references('id')->on('projects')
                 ->cascadeOnDelete();
-            $table->foreign('tecnology_id')->references('id')->on('technologies')
+            $table->foreign('technology_id')->references('id')->on('technologies')
                 ->cascadeOnDelete();
 
-            $table->primary(['project_id', 'tecnology_id']);
+            $table->primary(['project_id', 'technology_id']);
         });
     }
 
